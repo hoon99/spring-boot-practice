937ea5604831570d028ba39b80c47362235d8622
Jaehoon <gkdwngo@gmail.com>
2024-03-16 17:55:39 +0900

    feat: 멤버 Repository 추가

---
+++ src/main/java/sbp/springbootpractice/MemberRepository.java
@@ -0,0 +1,8 @@
+package sbp.springbootpractice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
