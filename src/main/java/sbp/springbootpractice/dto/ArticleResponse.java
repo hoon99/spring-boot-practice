4d57f564ccc2853e1de015163b5d3cc97dd43706
Jaehoon <gkdwngo@gmail.com>
2024-03-20 22:01:09 +0900

    feat: 게시물 응답 DTO 추가

---
+++ src/main/java/sbp/springbootpractice/dto/ArticleResponse.java
@@ -0,0 +1,16 @@
+package sbp.springbootpractice.dto;
+
+import lombok.Getter;
+import sbp.springbootpractice.domain.Article;
+
+@Getter
+public class ArticleResponse {
+
+    private final String title;
+    private final String content;
+
+    public ArticleResponse(Article article) {
+        this.title = article.getTitle();
+        this.content = article.getContent();
+    }
+}
