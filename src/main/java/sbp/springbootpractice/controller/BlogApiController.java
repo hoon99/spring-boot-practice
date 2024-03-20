748776954781a093a2c58f5887d82eef7354f614
Jaehoon <gkdwngo@gmail.com>
2024-03-20 22:01:45 +0900

    feat: findAllArticles 메서드 추가

--- src/main/java/sbp/springbootpractice/controller/BlogApiController.java
+++ src/main/java/sbp/springbootpractice/controller/BlogApiController.java
@@ -3,12 +3,16 @@ package sbp.springbootpractice.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import sbp.springbootpractice.domain.Article;
 import sbp.springbootpractice.dto.AddArticleRequest;
+import sbp.springbootpractice.dto.ArticleResponse;
 import sbp.springbootpractice.service.BlogService;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP Response Body에 객체 데이터를 JSON 형식으로 반환하는 컨트롤러
@@ -24,5 +28,16 @@ public class BlogApiController {
 
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(savedArticle);
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponse>> findAllArticles() {
+        List<ArticleResponse> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponse::new)
+                .toList();
+
+        return ResponseEntity.ok()
+                .body(articles);
     }
 }
